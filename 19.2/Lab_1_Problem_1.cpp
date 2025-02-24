23a50ed849a1523bfdd361cd50e52cbf10505c4b
PunkHazard125 <blackpool91323@gmail.com>
2025-02-24 22:14:58 +0600

    Initial Commit

---
+++ 19.2/Lab_1_Problem_1.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const string nl = "\n";
+
+int binarySearchNext(int array[], int size, int key) {
+
+    if (array[size - 1] < key)
+    {
+        return -1;
+    }
+    
+
+    int i = 0, j = size - 1, m;
+
+    while (i <= j)
+    {
+        m = (i + j) / 2;
+
+        if (array[m] == key)
+        {
+            return m + 1;
+        }
+        
+        if (key > array[m])
+        {
+            i = m + 1;
+        }
+        else
+        {
+            j = m - 1;
+        }
+    }
+
+    return j + 1;
+
+}
+
+int binarySearchPrevious(int array[], int size, int key) {
+
+    if (array[0] > key)
+    {
+        return -1;
+    }
+
+    int i = 0, j = size - 1, m;
+
+    while (i <= j)
+    {
+        m = (i + j) / 2;
+
+        if (array[m] == key)
+        {
+            return m - 1;
+        }
+        
+        if (key > array[m])
+        {
+            i = m + 1;
+        }
+        else
+        {
+            j = m - 1;
+        }
+    }
+
+    return i - 1;
+
+}
+
+int main(void) {
+
+    int n, q;
+
+    cin >> n;
+    int chimps[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> chimps[i];
+    }
+
+    cin >> q;
+    int ziggy[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ziggy[i];
+    }
+
+    for (int i = 0; i < q; i++)
+    {
+        int previous = binarySearchPrevious(chimps, n, ziggy[i]);
+        int next = binarySearchNext(chimps, n, ziggy[i]);
+
+        if (previous != -1)
+        {
+            cout << chimps[previous] << " ";
+        }
+        else
+        {
+            cout << "X" << " ";
+        }
+        
+        if (next != -1)
+        {
+            cout << chimps[next] << nl;
+        }
+        else
+        {
+            cout << "X" << nl;
+        }
+    }
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+Author: Mosabbir Ahmed
+
+Student ID: 2023-3-60-125
+
+Institution: East West University
+
+Course: CSE207 | Data Structures
+
+Langauge: C++ & Java
+
+Faculty: AASR
+
+Semester: Spring 2025
